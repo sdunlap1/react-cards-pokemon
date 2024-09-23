dc5e3583d8fe2ea0f190b54e69941aee7e71fb5e
sdunlap1 <sdunlap1@mac.com>
2024-09-22 19:36:52 -0700

    Implemented useAxios in PokeDex.js

--- react-cards-pokemon/src/PokeDex.js
+++ react-cards-pokemon/src/PokeDex.js
@@ -1,26 +1,24 @@
-import React, { useState } from "react";
-import {v1 as uuid} from "uuid";
-import axios from "axios";
+import React from "react";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
 import "./PokeDex.css";
+import { useAxios } from "./hooks"; // Import the hook
 
 /* Renders a list of pokemon cards.
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-  const [pokemon, setPokemon] = useState([]);
-  const addPokemon = async name => {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name}/`
-    );
-    setPokemon(pokemon => [...pokemon, { ...response.data, id: uuid() }]);
+  const [pokemon, addPokemon] = useAxios("https://pokeapi.co/api/v2/pokemon/"); // Use base URL
+
+  const handleAddPokemon = name => {
+    addPokemon(name); // Call the hook to add the pokemon by name
   };
+
   return (
     <div className="PokeDex">
       <div className="PokeDex-buttons">
         <h3>Please select your pokemon:</h3>
-        <PokemonSelect add={addPokemon} />
+        <PokemonSelect add={handleAddPokemon} />
       </div>
       <div className="PokeDex-card-area">
         {pokemon.map(cardData => (
@@ -39,5 +37,6 @@ function PokeDex() {
     </div>
   );
 }
+
 
 export default PokeDex;
